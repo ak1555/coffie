89f24ad6073622dd8f71a67d625e8524c88f3350
Nandanacb <nandhnacb@gmail.com>
2024-09-28 18:00:04 +0530

    save

---
+++ cfeshp/lib/components/mybottomnavbar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class Mybottomnavbar extends StatelessWidget {
+  void Function(int)? onTabChange;
+   Mybottomnavbar({super.key, required this.onTabChange});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GNav(
+        onTabChange: (value) {
+          return onTabChange!(value);
+        },
+        tabs: [
+        GButton(icon: Icons.home,
+        text: "Home",)
+      ,
+      GButton(icon: Icons.shop,
+      text: "Cart",)
+      ]),
+      
+    );
+  }
+}
---
+++ cfeshp/lib/pages/homepage.dart
@@ -0,0 +1,36 @@
+import 'package:cfeshp/components/mybottomnavbar.dart';
+import 'package:cfeshp/pages/cartpage.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+
+  int selectedpage =0;
+
+  final List<Widget>pages=[
+    HomePage(),CartPage()
+    ];
+    
+    void navigatebottombar(int index){
+      setState(() {
+        selectedpage=index;
+      });
+    }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      bottomNavigationBar: Mybottomnavbar(
+        onTabChange: (index) => navigatebottombar(index),
+      ),
+      body: pages[selectedpage],
+    );
+  }
+}
